78a5b0da1fd0c7600a2fc58ad2979cbac84812e5
tokyramarozaka <tokyramarozaka@gmail.com>
2026-07-06 14:19:40 +0300

    fix: syntax error on list in OrderTest

--- src/test/java/ishop/OrderTest.java
+++ src/test/java/ishop/OrderTest.java
@@ -18,14 +18,14 @@ public class OrderTest {
 
     @BeforeEach
     void setUp() {
-        supra = new Product(1, "Toyota Supra Rz", "A magnificant tuner car", VEHICLE, new ArrayList<PriceHistory>(){
+        supra = new Product(1, "Toyota Supra Rz", "A magnificant tuner car", VEHICLE, new ArrayList<PriceHistory>(
             new PriceHistory(1,BigDecimal.valueOf(58_000), Instant.parse("2025-01-01T00:00:00Z")),
             new PriceHistory(2, BigDecimal.valueOf(69_000), Instant.parse("2026-01-01T00:00:00Z"))
-        });
-        skyline = new Product(2, "Nissan Skyline GT-R 34", "The legendary vehicle from Fast and furious", VEHICLE, new ArrayList<PriceHistory>(){
+        );
+        skyline = new Product(2, "Nissan Skyline GT-R 34", "The legendary vehicle from Fast and furious", VEHICLE, new ArrayList<PriceHistory>(
             new PriceHistory(1, BigDecimal.valueOf(15_000), Instant.parse("2025-01-01T00:00:00Z")),
             new PriceHistory(2, BigDecimal.valueOf(30_000), Instant.parse("2025-06-01T00:00:00Z"))
-        });
+        );
         customer = new Customer(1, "CL001","Dominique", "Torreto","torreto@mail.com","azerty", "+123 456 789");
     }
     @Test
