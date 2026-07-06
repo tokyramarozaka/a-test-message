23ebdbb3d87b904e89959a354a60872534b8f222
tokyramarozaka <tokyramarozaka@gmail.com>
2026-07-06 13:57:35 +0300

    test: add test for Product and Order

---
+++ src/test/java/ishop/ProductTest.java
@@ -0,0 +1,30 @@
+package ishop;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ProductTest {
+    private Product product;
+    @BeforeEach
+    void setUp() {
+        product = new Product(1, "Sweater XL", "A thick sweater", CLOTHING, new ArrayList<PriceHistory>(
+                new PriceHistory(1, BigDecimal.valueOf(30_000), Instant.parse("2026-01-01T08:00:00Z")),
+                new PriceHistory(2, BigDecimal.valueOf(40_000), Instant.parse("2026-04-01T08:00:00Z")),
+                new PriceHistory(3, BigDecimal.valueOf(70_000), Instant.parse("2026-07-06T08:00:00Z"))
+        ));
+    }
+    @Test
+    void getCurrentPrice_returns_theLatestPrice() {
+        assertEquals(BigDecimal.valueOf(70_000), product.getCurrentPrice());
+    }
+    @Test
+    void getPriceAt_returns_theCorrectPrice() {
+        assertEquals(BigDecimal.valueOf(40_000) , product.getPriceAt(Instant.parse("2026-05-26T09:30:00Z")));
+        assertEquals(BigDecimal.valueOf(30_000) , product.getPriceAt(Instant.parse("2026-02-26T09:30:00Z")));
+    }
+}
